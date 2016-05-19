1fe6af265a97dacc1672f9f81369b2f8bc01d470
alerario <alerario@gmail.com>
2016-05-19 13:12:09 -0300

    Criacao do managed bean JsfCidades e modificacao do index

---
+++ src/MavenVideo/src/main/java/br/jsf/JsfCidades.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.jsf;
+
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+/**
+ *
+ * @author alexandre
+ */
+@ManagedBean
+@RequestScoped
+public class JsfCidades {
+
+    /**
+     * Creates a new instance of JsfCidades
+     */
+    public JsfCidades() {
+    }
+    
+    private int codigo;
+    private String nome;
+
+    public void salvar(){
+        br.data.entity.Cidades cid;
+        cid = new br.data.entity.Cidades();
+        cid.setCodigo(codigo);
+        cid.setNome(nome);
+        new br.data.crud.CrudCidades().persist(cid);
+    }
+    
+    public java.util.Collection<br.data.entity.Cidades> getAll(){
+        return new br.data.crud.CrudCidades().getAll();
+    }
+    
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
+}
